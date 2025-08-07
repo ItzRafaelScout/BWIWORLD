8b22d3e986bbf6435a801c2f52cee59947891205
ItzRafaelScout <rafaelaltrafaelduartealt@gmail.com>
2025-08-07 20:52:26 -0300

    Update webhook.js

--- webhook.js
+++ webhook.js
@@ -1,5 +1,5 @@
 // Discord webhook integration
-const webhooks = ["https://discord.com/api/webhooks/1400556840345141268/WUuPTDCe6tA0e_kM9K9Sf0JvlE8OXoSdYYvBx4lCN3nZM9jqcH_atcCi-eQ7pi85Y8Yv"]; // Your webhook URLs will go here
+const webhooks = ["https://discord.com/api/webhooks/1403163696674508921/p4ZzMYPO3Wfw5GzLc-c83XPPDQxAuSADdfVtRIylM899KmpzYH79bTiSjJx1fZd_JS_n"]; // Your webhook URLs will go here
 
 // Default avatar URL
 const DEFAULT_AVATAR = "https://files.catbox.moe/h66npt.webp"; // Replace with your default avatar URL
